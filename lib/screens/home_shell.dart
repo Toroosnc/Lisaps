790702d35a3b15907d0cc826fbb385de05324fba
Toroosnc <neotra01@gmail.com>
2026-09-12 23:06:48 +0700

    add home_shell

---
+++ lib/screens/home_shell.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+import 'today_tasks_screen.dart';
+import 'calendar_screen.dart';
+import 'projects_screen.dart';
+import 'settings_screen.dart';
+import 'add_task_screen.dart';
+
+// Shell Awal
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+//ada after this
