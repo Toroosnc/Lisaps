79db953a35da77fce532cd2afdcbe4dd63d7c661
Toroosnc <neotra01@gmail.com>
2026-09-15 20:47:28 +0700

    add buiild context

--- lib/screens/home_shell.dart
+++ lib/screens/home_shell.dart
@@ -12,4 +12,36 @@ class HomeShell extends StatefulWidget {
   @override
   State<HomeShell> createState() => _HomeShellState();
 }
-//ada after this
+class _HomeShellState extends State<HomeShell> {
+  int _index = 0;
+
+  final _screens = const [
+    TodayTasksScreen(),
+    CalendarScreen(),
+    ProjectsScreen(),
+    SettingsScreen(),
+  ];
+
+  void _openAddTask() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => const AddTaskScreen()),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    // _index 2 dan 3 dipakai untuk Proyek & Setelan; posisi ke-2 di nav
+    // (indeks 2 visual) adalah tombol tambah, jadi kita map manual.
+    final navIndexToScreen = [0, 1, 2, 3];
+    return Scaffold(
+      body: IndexedStack(
+        index: navIndexToScreen[_index],
+        children: _screens,
+      ),
+      bottomNavigationBar: _BottomNav(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        onAddTap: _openAddTask,
+      ),
+    );
+  }
+}
