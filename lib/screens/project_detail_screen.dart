6cb8ff503f8cc966f9fa7ec7ae8ba3e453c64484
Toroosnc <neotra01@gmail.com>
2026-09-15 20:50:02 +0700

    add imprt for detail screen

---
+++ lib/screens/project_detail_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/task_provider.dart';
+import '../theme/app_theme.dart';
+import '../widgets/task_card.dart';
