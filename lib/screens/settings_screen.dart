48cebe1c5ce4949ee9b557d80f9ce3f411053d51
Toroosnc <neotra01@gmail.com>
2026-09-19 03:18:04 +0700

    add setting screen dart

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+//add import
